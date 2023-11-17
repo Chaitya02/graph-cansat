2cc12e390d27fafbd98384ae6c44b823e5400432
Chaitya <chaityadobariya02@gmail.com>
2023-11-17 10:16:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Plot from 'react-plotly.js';
 
-function App() {
+// Generate random points to plot on X-axis (sensor data)
+const getData = () => {
+  // Points generated between 0 - 10
+  return Math.round(Math.random() * 10);
+};
+
+// Create the Plot
+const MyPlot = () => {
+  const maxDataPoints = 8; // Show max point on X-axis
+
+  // Initial Plot
+  const [trace1, setTrace1] = useState({
+    x: [0, 1, 2, 3, 4, 5],
+    y: [getData(), getData(), getData(), getData(), getData(), getData()],
+    type: 'line',
+    mode: 'lines+markers',
+  });
+
+  // Initial Layout
+  const [layout, setLayout] = useState({
+    width: 600,
+    height: 500,
+    title: 'My Plot',
+    xaxis: {
+      range: [maxDataPoints - 8, maxDataPoints],
+    },
+  });
+
+  // Update the graph
+  useEffect(() => {
+
+    // Interval Logic
+    const intervalId = setInterval(() => {
+      setTrace1((prevTrace) => {
+        // Generate new data to plot further
+        const newX = [...prevTrace.x, prevTrace.x.length];
+        const newY = [...prevTrace.y, getData()];
+
+        return { ...prevTrace, x: newX, y: newY };
+      });
+
+      setLayout((prevLayout) => {
+        // Shift the layout further
+        const newRange = [prevLayout.xaxis.range[0] + 1, prevLayout.xaxis.range[1] + 1];
+        return { ...prevLayout, xaxis: { ...prevLayout.xaxis, range: newRange } };
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const data = [trace1];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Plot
+      data={data}
+      layout={layout}
+    />
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <MyPlot />
     </div>
   );
-}
+};
 
 export default App;
